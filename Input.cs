81f41a9a25eb278bb6e36230567bd63c2a6cfd94
Aleksi <Aleksi@DESKTOP-QK2T0B6>
2021-08-03 17:09:12 +0300

    Add project files.

---
+++ Input.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections.Generic;
+
+public class Input
+{
+    //User32 Imports
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetMessageExtraInfo();
+
+    //Input Struct
+    [StructLayout(LayoutKind.Sequential)]
+    private struct KeyboardInput
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MouseInput
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    private struct HardwareInput
+    {
+        public uint uMsg;
+        public ushort wParamL;
+        public ushort wParamH;
+    }
+    [StructLayout(LayoutKind.Explicit)]
+    private struct InputUnion
+    {
+        [FieldOffset(0)] public MouseInput mi;
+        [FieldOffset(0)] public KeyboardInput ki;
+        [FieldOffset(0)] public HardwareInput hi;
+    }
+    private struct INPUT
+    {
+        public int type;
+        public InputUnion u;
+    }
+
+    //Flags
+    [Flags]
+    private enum InputType
+    {
+        Mouse = 0,
+        Keyboard = 1,
+        Hardware = 2
+    }
+    [Flags]
+    private enum KeyEventF
+    {
+        KeyDown = 0x0000,
+        ExtendedKey = 0x0001,
+        KeyUp = 0x0002,
+        Unicode = 0x0004,
+        Scancode = 0x0008
+    }
+    [Flags]
+    private enum MouseEventF
+    {
+        Absolute = 0x8000,
+        HWheel = 0x01000,
+        Move = 0x0001,
+        MoveNoCoalesce = 0x2000,
+        LeftDown = 0x0002,
+        LeftUp = 0x0004,
+        RightDown = 0x0008,
+        RightUp = 0x0010,
+        MiddleDown = 0x0020,
+        MiddleUp = 0x0040,
+        VirtualDesk = 0x4000,
+        Wheel = 0x0800,
+        XDown = 0x0080,
+        XUp = 0x0100
+    }
+
+    private Dictionary<char, ushort> keyCodes = new Dictionary<char, ushort>() { { 'a', 0x1E }, { 'b', 0x30 }, { 'c', 0x2E }, { 'd', 0x20 }, { 'e', 0x12 }, { 'f', 0x21 }, { 'g', 0x22 }, { 'h', 0x23 }, { 'i', 0x17 }, { 'j', 0x24 }, { 'k', 0x25 }, { 'l', 0x26 }, { 'm', 0x32 }, { 'n', 0x31 }, { 'o', 0x18 }, { 'p', 0x19 }, { 'q', 0x10 }, { 'r', 0x13 }, { 's', 0x1F }, { 't', 0x14 }, { 'u', 0x16 }, { 'v', 0x2F }, { 'w', 0x11 }, { 'x', 0x2D }, { 'y', 0x15 }, { 'z', 0x2C } };
+
+    public bool KeyDown(string keys)
+    {
+        INPUT[] input = new INPUT[keys.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!Char.IsLetter(keys[i]))
+                return false;
+            input[i] = new INPUT
+            {
+                type = (int)InputType.Keyboard,
+                u = new InputUnion
+                {
+                    ki = new KeyboardInput
+                    {
+                        wVk = 0,
+                        wScan = keyCodes[Char.ToLower(keys[i])],
+                        dwFlags = (uint)(KeyEventF.KeyDown | KeyEventF.Scancode),
+                        dwExtraInfo = GetMessageExtraInfo()
+                    }
+                }
+            };
+        }
+        SendInput((uint)input.Length, input, Marshal.SizeOf(typeof(INPUT)));
+        return true;
+    }
+
+    public bool KeyUp(string keys)
+    {
+        INPUT[] input = new INPUT[keys.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!Char.IsLetter(keys[i]))
+                return false;
+            input[i] = new INPUT
+            {
+                type = (int)InputType.Keyboard,
+                u = new InputUnion
+                {
+                    ki = new KeyboardInput
+                    {
+                        wVk = 0,
+                        wScan = keyCodes[Char.ToLower(keys[i])],
+                        dwFlags = (uint)(KeyEventF.KeyUp | KeyEventF.Scancode),
+                        dwExtraInfo = GetMessageExtraInfo()
+                    }
+                }
+            };
+        }
+        SendInput((uint)input.Length, input, Marshal.SizeOf(typeof(INPUT)));
+        return true;
+    }
+
+    public bool KeyPress(string keys, int time = 0)
+    {
+        if (!KeyDown(keys))
+            return false;
+        Thread.Sleep(time);
+        if (!KeyUp(keys))
+            return false;
+        return true;
+    }
+}
---
+++ Osubot.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Osubot
+{
+    class Osubot : Form
+    {
+        //DLL Imports
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        //Public Vars
+        public static Color[] colors = new Color[] { Color.FromArgb(255, 0, 255, 0), Color.FromArgb(255, 0, 0, 255), Color.FromArgb(255, 0, 254, 0) };
+        public static int[] pixelPositions = new int[] { 340, 407, 475, 542 };
+        public static char[] chars = new char[] { 'd', 'f', 'j', 'k' };
+
+        public static bool[] lockedKey = new bool[4];
+        public static bool[] pushedLong = new bool[4];
+        public static bool[] upLong = new bool[4];
+
+        static Stopwatch stopWatch = new Stopwatch();
+
+        //Start Methods
+        public static Color[] GetColors()
+        {
+            Color[] cols = new Color[4];
+
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            stopWatch.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                int a = (int)GetPixel(dc, pixelPositions[i], 577);
+                cols[i] = Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+            }
+            ReleaseDC(desk, dc);
+            stopWatch.Stop();
+
+            return cols;
+        }
+
+        public static void PushOutputThread(object output)
+        {
+            Input input = new Input();
+            Thread.Sleep(775);
+            //Console.WriteLine(output);
+            input.KeyPress(output.ToString(), 8);
+        }
+
+        public static void PushKeyDown(object output)
+        {
+            Input input = new Input();
+            Thread.Sleep(775);
+            Console.WriteLine(output.ToString() + "Down");
+            input.KeyDown(output.ToString());
+        }
+        public static void PushKeyUp(object output)
+        {
+            Input input = new Input();
+            Thread.Sleep(775);
+            Console.WriteLine(output.ToString() + "UP");
+            input.KeyUp(output.ToString());
+        }
+
+        public static void LongOutput()
+        {
+            Input input = new Input();
+            string on;
+            string off;
+
+            while (true)
+            {
+                Thread ton = new Thread(new ParameterizedThreadStart(PushKeyDown));
+                Thread toff = new Thread(new ParameterizedThreadStart(PushKeyUp));
+
+                on = "";
+                off = "";
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (pushedLong[i] && !lockedKey[i])
+                    {
+                        lockedKey[i] = true;
+                        upLong[i] = false;
+                        on += chars[i];
+                    }
+                    else if (!pushedLong[i] && !upLong[i])
+                    {
+                        off += chars[i];
+                        upLong[i] = true;
+                    }
+                }
+
+                if (on != "")
+                    ton.Start(on);
+                if (off != "")
+                    toff.Start(off);
+
+                Thread.Sleep(8);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Thread longOutput = new Thread(new ThreadStart(LongOutput));
+            //longOutput.Start();
+            string output;
+
+            while (true)
+            {
+                stopWatch.Reset();
+                //stopWatch.Start();
+                Thread t = new Thread(new ParameterizedThreadStart(PushOutputThread));
+                output = "";
+
+                Color[] color = GetColors();
+                for (int i = 0; i < 4; i++)
+                {
+                    if (color[i] == colors[0])
+                    {
+                        if (!lockedKey[i])
+                        {
+                            lockedKey[i] = true;
+                            output += chars[i];
+                            continue;
+                        }
+                        continue;
+                    }
+                    if (color[i] == colors[1])
+                    {
+                        pushedLong[i] = true;
+                        continue;
+                    }
+                    lockedKey[i] = false;
+                    pushedLong[i] = false;
+                }
+
+                if (output != "")
+                {
+                    t.Start(output);
+                }
+
+                Thread.Sleep(0);
+                //stopWatch.Stop();
+                Console.WriteLine(stopWatch.ElapsedMilliseconds);
+            }
+        }
+    }
+}
